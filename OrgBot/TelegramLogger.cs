e3065fdac85c4f4e43b0d36468480482527f9164
yurik <yurik@satec-global.com>
2024-10-07 17:53:59 +0300

    Added file logger

--- OrgBot/TelegramLogger.cs
+++ OrgBot/TelegramLogger.cs
@@ -6,9 +6,10 @@ public class TelegramLogger(List<string> actionList, uint logSize) : ILogger
 {
     private List<string> ActionLog { get; } = actionList;
     private uint LogSize { get; } = logSize;
+    private readonly string _logFilePath = Path.Combine(".", "data", "bot_log.txt");
+
     public IDisposable BeginScope<TState>(TState state) where TState : notnull => null!;
     public bool IsEnabled(LogLevel logLevel) => true;
-
 
     public async Task LogInformationAsync(string message) => await LogAsync(LogLevel.Information, message);
     public async Task LogErrorAsync(string message) => await LogAsync(LogLevel.Error, message);
@@ -23,6 +24,8 @@ public class TelegramLogger(List<string> actionList, uint logSize) : ILogger
             await Console.Error.WriteLineAsync(formattedMessage);
         else
             await Console.Out.WriteLineAsync(formattedMessage);
+
+        await File.AppendAllLinesAsync(_logFilePath, [formattedMessage]);
     }
 
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
@@ -41,5 +44,4 @@ public class TelegramLogger(List<string> actionList, uint logSize) : ILogger
             }
         }
     }
-
 }
